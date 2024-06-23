9919bb9f07c82a3214aa295a499853ab5e245007
mohit <mohit@pierian.co>
2024-06-23 22:05:42 +0530

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   presets: [
-    'module:@react-native/babel-preset',
     'module:metro-react-native-babel-preset'
   ],
   plugins: [
     'react-native-reanimated/plugin',
+    "transform-react-jsx-source",
+    "transform-react-jsx-self"
   ],
 };
+
 
